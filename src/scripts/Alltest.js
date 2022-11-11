27b84be8866c8f47734fdeffa0df000630db6f98
MMonu01 <mdmonu2021@gmail.com>
2022-11-12 00:53:54 +0530

    new commit

--- src/scripts/Alltest.js
+++ src/scripts/Alltest.js
@@ -1,7 +1,29 @@
 import {apall} from '../components/AlltestApend.js'
 import {navbar} from '../components/tlnavbar.js'
+import {footer} from "../components/footer.js"
+
 let nav = document.getElementById("navbar")
 nav.innerHTML = navbar()
+
+
+let myBtn = document.getElementById("myBtn")
+myBtn.onclick = ()=>{
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myBtn.style.display = "block";
+  } else {
+    myBtn.style.display = "none";
+  }
+}
+
+
 
 const fetch_data = async()=>{
     const response = await fetch("http://localhost:1010/admin/monu")
@@ -10,3 +32,7 @@ const fetch_data = async()=>{
    console.log(refine_data.Lab_Tests[0])
 }
 fetch_data()
+
+
+let foot = document.getElementById("footer")
+foot.innerHTML = footer()
