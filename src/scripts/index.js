b15f5d70adae5fdbbf0a214eccb96df658b6e3c6
Abhishek Singh <sabhishek900@gmail.com>
2022-11-13 00:04:21 +0530

    getting there...please wait

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,43 @@
+
+import {showForm} from "./showForm.js";
+import {logout} from "./auth.js"
+
+// cart items display
+let ls_cart = JSON.parse(localStorage.getItem("ls_cart")) || null;
+// ls_cart = [1,2,3,4]
+if(ls_cart!=null){
+    let number = ls_cart.length;
+    document.querySelector(".carticon>span").innerHTML = number;
+    document.querySelector(".carticon>span").style.display = "inline-block"
+}
+
+let user = JSON.parse(localStorage.getItem("user_details")) || null;
+
+if(user!=null){
+    let log_in = document.querySelector(".logintext>span:first-child");
+    let log_out = document.querySelector("#LogOut");
+    log_in.innerHTML = user.full_name.split(" ")[0];
+    log_out.style.display = "inline-block";
+    log_out.onclick = ()=>{
+        logout()
+    window.location.reload()
+
+    }
+};
+
+document.querySelector(".personalcareP").onclick = ()=>{
+    window.location.href = "personal_care.html"
+}
+document.querySelector(".skincareP").onclick = ()=>{
+    window.location.href = "skin_care.html"
+}
+document.querySelector(".beautyP").onclick = ()=>{
+    window.location.href = "beauty.html"
+}
+document.querySelector(".homecareP").onclick = ()=>{
+    window.location.href = "home_care.html"
+}
+
 // show and hide navbar
 
 window.onscroll = () => {
@@ -10,142 +50,123 @@ window.onscroll = () => {
     }
 }
 
-// slider for offer
-let body = document.querySelector("body");
-body.onload = showoffer();
- body.onload = showpaymentoffer();
+//lab Test link
+let lbtest = document.getElementById("LabTest")
+lbtest.onclick = () => {
+    window.location.href = "Test_lab.html";
+};
 
 
-let leftarrow = document.querySelector("#ltarrow");
-leftarrow.addEventListener("click", moveleft);
+// Login form popup
+document.querySelector(".login").onclick= async () => {
+ let res =  await showForm("login");
+ console.log('res',res);
 
-let rightarrow = document.querySelector("#rtarrow");
-rightarrow.addEventListener("click", moveright);
-
-
-// let left =1;
-// let right=2;
-function showoffer() {
-    let left =1;
-    let right=2;
-
-    for(let i=left; i<=right; i++){
-        document.getElementById("c"+i).style.display = "inline-block";
-    
-    }
-    
-}
-let left =1;
-let right=2;
-function moveleft() {
-    // let left =1;
-    // let right=2;
-    console.log("ltclicked")
-  if(left <= 1 && right<= 2){
-   document.getElementById("c"+left).style.display = "none";
-   left +=1;
-   right +=1;
-
-      for(let i=left; i<=right; i++){
-       document.getElementById("c"+i).style.display = "inline-block";
-      }
- } else {
-   return; 
-}
-}
-
-
-function moveright() {
-    // let left =1;
-    // let right=2;
-   console.log("rtclicked")
-  if(left >= 2 && right >= 3){
-   document.getElementById("c"+right).style.display = "none";
-   left -=1;
-   right -=1;
-
-   for(let i=left; i<=right; i++){
-    document.getElementById("c"+i).style.display = "inline-block";
-   }
-} else {
-   return; 
-}
-}
-
-
-//
-let payleftarrow = document.querySelector("#payleft");
-payleftarrow.addEventListener("click", paymoveleft);
-
-let payrightarrow = document.querySelector("#payright");
-payrightarrow.addEventListener("click", paymoveright);
-
-function showpaymentoffer(){
-
-    let left =1;
-    let right=3;
-
-    for(i=left; i<=right; i++){
-        document.getElementById("p"+i).style.display = "inline-block";
-    }
-}
-
-let pleft=1;
-let pright=3;
-
-function paymoveleft(){
-    //console.log("clicked");
-    if(pleft <= 3 && pright<= 6){
-        document.getElementById("p"+left).style.display = "none";
-        pleft +=1;
-        pright +=1;
-     
-           for(let i=pleft; i<=pright; i++){
-            document.getElementById("p"+i).style.display = "inline-block";
-           }
-      } else {
-        return; 
-     }
-
-}
-
-function paymoveright() {
-   //console.log("rtclicked")
-  if(pleft >= 2 && pright >= 5){
-   document.getElementById("p"+pright).style.display = "none";
-   pleft -=1;
-   pright -=1;
-
-   for(let i=pleft; i<=pright; i++){
-    document.getElementById("p"+i).style.display = "inline-block";
-   }
-} else {
-   return; 
-}
+ if(res.status === "success"){
+    window.location.reload()
+ }
+ 
 }
 
 
 
-let count =0;
-    document.querySelector("cat_left").onclick = () => {
+
+
+let count4 =0;
+    document.querySelector("#ltarrow").onclick = () => {
         console.log("clicked")
         
-        if(count =0){
+        if(count4 <= 0){
             return;
         }
-        count--;
-        let val = count * 10;
-        document.getElementById("categories_container").style.transform = `translateX(${val}vw)`;
+        count4--;
+        let val = count4 * 45;
+        document.querySelector(".container").style.transform = `translateX(${val}vw)`;
 
     };
 
-    document.querySelector("cat_right").onclick = () => {
+    document.querySelector("#rtarrow").onclick = () => {
 
-        if(count * 10 > 100){
+        if(count4  > 0){
+           return;
+        }
+        count4++;
+        let val =count4 *45;
+        document.querySelector(".container").style.transform = `translateX(-${val}vw)`;
+    };
+
+
+    /////////////////////////////////////////////////////////////////////////////
+
+let count =0;
+    document.querySelector("#payleft").onclick = () => {
+        console.log("clicked")
+        
+        if(count ==0){
+            return;
+        }
+        count--;
+        let val = count * 30;
+        document.querySelector(".payment_offer_container").style.transform = `translateX(-${val}vw)`;
+
+    };
+
+    document.querySelector("#payright").onclick = () => {
+
+        if(count *30 > 100){
            return;
         }
         count++;
-        let val =count *10;
-        document.getElementById("categories_container").style.transform = `translateX(-${val}vw)`;
+        let val =count *30;
+        document.querySelector(".payment_offer_container").style.transform = `translateX(-${val}vw)`;
+    };
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+let count1 =0;
+    document.querySelector("#cat_left").onclick = () => {
+        console.log("clicked")
+        
+        if(count1 ==0){
+            return;
+        }
+        count1--;
+        let val = count1 * 120;
+        document.querySelector("#categories_container").style.transform = `translateX(-${val}px)`;
+
+    };
+
+    document.querySelector("#cat_right").onclick = () => {
+
+        if(count1 > 4){
+           return;
+        }
+        count1++;
+        let val =count1 *120;
+        document.querySelector("#categories_container").style.transform = `translateX(-${val}px)`;
+    };
+
+//3
+    let count2 =0;
+    document.querySelector("#category_left").onclick = () => {
+        console.log("clicked")
+        
+        if(count2 ==0){
+            return;
+        }
+        count2--;
+        let val = count2 * 30;
+        document.querySelector("#newLaunch_container").style.transform = `translateX(${val}vw)`;
+
+    };
+
+    document.querySelector("#category_right").onclick = () => {
+
+        if(count2 *30 > 5){
+           return;
+        }
+        count2++;
+        let val =count2 *30;
+        document.querySelector("#newLaunch_container").style.transform = `translateX(-${val}vw)`;
     };
 
